99371aac19adac945382c3045453601712e15829
Job Job <necronomite@gmail.com>
2016-12-17 23:14:31 +0800

    Create crsa.js
    Open Developer Tools in Chrome using CTRL+SHIFT+I or F12
    Open the "Console" tab
    Paste the upper code block and hit Enter.
    Paste the lower code block and hit Enter.
    
    The entire teacher rating page should have all clicked SA's

---
+++ crsa.js
@@ -0,0 +1,11 @@
+var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+
+$("input[onclick$='1)']").each(function( index ) {
+this.click();
+});
